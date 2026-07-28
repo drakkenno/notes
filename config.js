4aeaec1c129db16d5ae79078c6ee20a551c6b76b
drakkenno <drakkenno@gmail.com>
2026-07-28 17:41:40 +0200

    Fixing auth

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // ============================================================
 //  CONFIGURATION - Set your Vercel API URL here
 // ============================================================
-const VERCEL_API_URL = 'hhttps://notes-3km3s0hqp-drakenotes1.vercel.app//api/notes';
-const AUTH_API_URL = 'hhttps://notes-3km3s0hqp-drakenotes1.vercel.app//api/auth';
+const VERCEL_API_URL = 'https://notes-3km3s0hqp-drakenotes1.vercel.app/api/notes';
+const AUTH_API_URL = 'https://notes-3km3s0hqp-drakenotes1.vercel.app/api/auth';
 
 // ============================================================
 //  STATE
